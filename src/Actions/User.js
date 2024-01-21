a48dab37fdc70e4e6d571a7debdbc133f7219116
shiv <yshiv4573@gmail.com>
2024-01-22 00:41:45 +0530

    finally commited

--- src/Actions/User.js
+++ src/Actions/User.js
@@ -117,6 +117,7 @@ export const signupuser =
           },
         }
       );
+      Cookies.set("token", data.token, { expires: 30 });
       dispatch(LoadUserSuccess(data.user));
     } catch (error) {
       dispatch(LoginFailure(error.response.data.message));
